71bdd26494d03eec4ce027231723d3764ec31fbe
lazar996 <lazarmax014@gmail.com>
2020-09-04 06:44:17 +0200

    brlja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,18 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { UsersComponent } from './users/users.component';
 import { LoginDialog } from './dialog/login.dialog';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { ContactsComponent } from './contacts/contacts.component'
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { ContactslistComponent } from './contactslist/contactslist.component'
 import { from } from 'rxjs';
-=======
->>>>>>> 441d37ad60e26c0ca17bdf476e3074cfd4366fdb
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     LoginDialog,
-<<<<<<< HEAD
     ContactsComponent,
     ContactslistComponent
     
-=======
->>>>>>> 441d37ad60e26c0ca17bdf476e3074cfd4366fdb
     
     
   ],
@@ -41,14 +35,8 @@ import { from } from 'rxjs';
     
     
   ],
-<<<<<<< HEAD
   entryComponents:[LoginDialog,ContactsComponent,ContactslistComponent],
   providers:[ { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } }, HttpClient],
   bootstrap: [AppComponent,]
-=======
-  entryComponents:[LoginDialog],
-  providers:[HttpClient],
-  bootstrap: [AppComponent]
->>>>>>> 441d37ad60e26c0ca17bdf476e3074cfd4366fdb
 })
 export class AppModule { }
